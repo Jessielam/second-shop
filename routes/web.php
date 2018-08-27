70da3976ef918af57dda2ecc32dd99a42ccd91dd
Unknown <960791428@qq.com>
2018-08-27 18:10:37 +0800

    feat: 用户登录注册
    * 注册时发送验证邮件
    * 可以使用手机号或者是邮箱登录

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,14 @@
 */
 
 Route::get('/', 'IndexController@index')->name('root');
+Auth::routes();
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/email_verify_notice', 'IndexController@emailVerifyNotice')->name('email_verify_notice');
+    Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
+    Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
+
+    Route::group(['middleware' => 'email_verified'], function() {
+        // 删掉这里面的内容
+    });
+});
